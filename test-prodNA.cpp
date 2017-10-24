42a696d181d891e0302038ec96d41a6e5111f32a
Florian Privé <florian.prive.21@gmail.com>
2017-10-24 22:54:18 +0200

    save

---
+++ test-prodNA.cpp
@@ -0,0 +1,64 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+/******************************************************************************/
+
+// [[Rcpp::export]]
+NumericVector pMatVec4(const NumericMatrix& macc, const NumericVector& x) {
+  
+  int n = macc.nrow();
+  int m = macc.ncol();
+  
+  IntegerVector m_nona(n, m);
+  NumericVector res(n);
+  int i, j;
+  double y;
+  
+  for (j = 0; j < m; j++) {
+    for (i = 0; i < n; i++) {
+      y = macc(i, j);
+      if (R_IsNA(y)) {
+        m_nona[i]--;
+      } else {
+        res[i] += x[j] * y;
+      }
+    }
+  }
+  for (i = 0; i < n; i++) res[i] /= sqrt(m_nona[i]);
+  
+  return res;
+}
+
+/******************************************************************************/
+
+// [[Rcpp::export]]
+NumericVector cpMatVec4(const NumericMatrix& macc, const NumericVector& x) {
+  
+  int n = macc.nrow();
+  int m = macc.ncol();
+  
+  NumericVector res(m);
+  double tmp, y;
+  int i, j, n_nona;
+  
+  // WARNING: do not use std::size_t because of `n - 4`
+  for (j = 0; j < m; j++) {
+    tmp = 0;
+    n_nona = n;
+    for (i = 0; i < n; i++) {
+      y = macc(i, j);
+      if (R_IsNA(y)) {
+        n_nona--;
+      } else {
+        tmp += y * x[i];
+      }
+    }
+    res[j] = tmp / sqrt(n_nona);
+  }
+  
+  return res;
+}
