3297e16e880224de1a9615f5f49e3dec35dd1241
Florian <florian.prive.21@gmail.com>
2017-11-16 23:49:44 +0100

    add from where

--- analytics-google.R
+++ analytics-google.R
@@ -1,10 +1,8 @@
-install.packages("googleAnalyticsR", dependencies = TRUE)
-
+# install.packages("googleAnalyticsR", dependencies = TRUE)
 library(googleAnalyticsR)
 ga_auth()
 
-account_list <- ga_account_list()
-account_list
+# (account_list <- ga_account_list())
 
 data_blog <- google_analytics_4(
   viewId = "152155542",
@@ -58,7 +56,7 @@ data_about %>%
   labs(x = "Date", y = "Nombre of sessions sur ma page") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-dplyr::right_join(data_blog, data_about, by = "date") %>%
+right_join(data_blog, data_about, by = "date") %>%
   ggplot(aes(sessions.x, sessions.y)) %>%
   bigstatsr:::MY_THEME() +
   geom_point(size = 2) +
@@ -71,3 +69,20 @@ dplyr::right_join(data_blog, data_about, by = "date") %>%
 filter(data_about, sessions > 6)
 #1: 2017-09-12 -> scrapping some medical -> weird
 #2: 2017-10-09 -> tuto PCA? ven 6 oct -> lun 9 oct
+
+
+
+data_about_country <- google_analytics_4(
+  viewId = "153879696",
+  date_range = lubridate::ymd("2017-06-26", Sys.Date()),
+  dimensions = c("country"),  # , "pagePath", "hour", "medium"
+  metrics = c("sessions")  # , "pageviews"
+)
+
+
+data_about_country %>%
+  mutate(country2 = ifelse(sessions < 5, "Others", country)) %>%
+  ggplot(aes(country2, sessions)) %>%
+  bigstatsr:::MY_THEME() +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
